6b321a2a75173ce87988c3f21ffc2721dd7fddfe
rizin777 <richardbernardo781@gmail.com>
2025-07-07 14:47:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "Bem-vindo à minha página! Continue estudando e nunca desista!";
+}
